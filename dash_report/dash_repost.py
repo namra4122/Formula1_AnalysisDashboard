b16144aeaea03386e898e28ba98e3b9f58aa78c8
Namra <namra4122@gmail.com>
2023-06-21 17:22:08 +0530

    MAP ADDED

--- dash_report/dash_repost.py
+++ dash_report/dash_repost.py
@@ -30,14 +30,14 @@ def fetch_season_data():
 # Fetch race_data
 def fetch_race_data(selected_value):
     race_api = f"https://ergast.com/api/f1/{selected_value}.json"
-    race_data = []
-
     races = requests.get(race_api).json()["MRData"]["RaceTable"]["Races"]
 
+    global race_data
+    race_data = {}
     for race in races:
-        race_data.append(race["raceName"])
+        race_data.update({race["raceName"]: race["Circuit"]["circuitId"]})
 
-    return race_data
+    return list(race_data.keys())
 
 
 app.layout = html.Div(
@@ -52,12 +52,11 @@ app.layout = html.Div(
         ),
         html.Br(),
         html.H2("Select Race Name :"),
-        # html.Label("Dropdown 2"),
         dcc.Dropdown(
             id="race_dropdown", value=None, style={"height": "30px", "width": "250px"}
         ),
         html.Br(),
-        # html.
+        html.Iframe(id="circuit_map", style={"height": "300px", "width": "10%"}),
     ],
     style={"padding-left": "15px", "padding-top": "15px"},
 )
@@ -74,17 +73,28 @@ def update_race_value(season_value):
         return race_value
 
 
-# @app.callback(Output(), Input())
-# def update_circuitImg(name, locality, country):
-#     geocode_url = f"https://nominatim.openstreetmap.org/search?q={name}+{locality}+{country}&format=json"
-#     response_json = requests.get(geocode_url).json()
-#     if len(response_json) > 0:
-#         location_data = response_json[0]
-#         circuit_lat = float(location_data["lat"])
-#         circuit_lon = float(location_data["lon"])
+@app.callback(
+    Output("circuit_map", "src"),
+    Input("race_dropdown", "value"),
+    prevent_initial_call=True,
+)
+def update_circuitImg(race_value):
+    circuit_api = f"http://ergast.com/api/f1/circuits/{race_data[race_value]}.json"
+    circuit = requests.get(circuit_api).json()["MRData"]["CircuitTable"]["Circuits"]
 
-#     osm_url = f"https://www.openstreetmap.org/export/embed.html?bbox={circuit_lon-0.01},{circuit_lat-0.01},{circuit_lon+0.01},{circuit_lat+0.01}&layer=mapnik"
-#     return osm_url
+    name = circuit[0]["circuitId"]
+    locality = circuit[0]["Location"]["locality"]
+    country = circuit[0]["Location"]["country"]
+
+    geocode_url = f"https://nominatim.openstreetmap.org/search?q={name}+{locality}+{country}&format=json"
+    response_json = requests.get(geocode_url).json()
+    if len(response_json) > 0:
+        location_data = response_json[0]
+        circuit_lat = float(location_data["lat"])
+        circuit_lon = float(location_data["lon"])
+
+    osm_url = f"https://www.openstreetmap.org/export/embed.html?bbox={circuit_lon-0.0075},{circuit_lat-0.0075},{circuit_lon+0.0075},{circuit_lat+0.0075 }&layer=mapnik"
+    return osm_url
 
 
 if __name__ == "__main__":
--- test.py
+++ test.py
@@ -1,23 +1,23 @@
 import requests
-import pandas as pd
-
-# race_df = pd.DataFrame()
 
 
-def fetch_race_data(selected_value):
-    race_api = f"https://ergast.com/api/f1/{selected_value}.json"
-    race_data = []
-    races = requests.get(race_api).json()["MRData"]["RaceTable"]["Races"]
+def update_circuitImg(race_value):
+    circuit_api = f"http://ergast.com/api/f1/circuits/{race_value}.json"
+    circuit = requests.get(circuit_api).json()["MRData"]["CircuitTable"]["Circuits"]
 
-    for race in races:
-        race_data.append(
-            {"raceName": race["raceName"], "circuitID": race["Circuit"]["circuitId"]}
-        )
+    name = circuit[0]["circuitId"]
+    locality = circuit[0]["Location"]["locality"]
+    country = circuit[0]["Location"]["country"]
 
-    temp_df = pd.DataFrame(race_data)
-    race_df = race_df.append(temp_df)
+    geocode_url = f"https://nominatim.openstreetmap.org/search?q={name}+{locality}+{country}&format=json"
+    response_json = requests.get(geocode_url).json()
+    if len(response_json) > 0:
+        location_data = response_json[0]
+        circuit_lat = float(location_data["lat"])
+        circuit_lon = float(location_data["lon"])
 
-    return race_df
+    osm_url = f"https://www.openstreetmap.org/export/embed.html?bbox={circuit_lon-0.01},{circuit_lat-0.01},{circuit_lon+0.01},{circuit_lat+0.01}&layer=mapnik"
+    return osm_url
 
 
-print(fetch_race_data(2023))
+print(update_circuitImg("monza"))
