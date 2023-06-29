80412b7a27bf5a01a3315b3e564c8b07736f819d
Namra <namra4122@gmail.com>
2023-06-29 18:00:49 +0530

    New ipynb update

--- test.py
+++ test.py
@@ -1,31 +1,10 @@
-import requests
+fast_lap = race_data.laps.pick_fastest()
 
-
-def fetch_race_data(selected_value):
-    race_api = f"https://ergast.com/api/f1/{selected_value}.json"
-    races = requests.get(race_api).json()["MRData"]["RaceTable"]["Races"]
-
-    global race_data
-    race_data = {}
-    for race in races:
-        race_data.update({race["raceName"]: race["round"]})
-
-    return race_data
-
-
-def update_circuitImg(season_value, race_value):
-    circuit_api = (
-        f"http://ergast.com/api/f1/{season_value}/{race_data[race_value]}/circuits.json"
-    )
-    circuit = requests.get(circuit_api).json()["MRData"]["CircuitTable"]["Circuits"]
-
-    name = circuit[0]["circuitId"]
-    locality = circuit[0]["Location"]["locality"]
-    country = circuit[0]["Location"]["country"]
-
-    print(name, locality, country)
-
-
-print(fetch_race_data(2022))
-
-update_circuitImg(2022, "Bahrain Grand Prix")
+print(f"Fastest Lap Driver: {fast_lap['Driver']}")
+print(f"Fastest Driver Team: {fast_lap['Team']}")
+print(f"Fastest Driver Position: {fast_lap['Position']}")
+print(f"Fastest LapTime: {fast_lap['LapTime']}")
+print(f"Fastest LapNumber: {fast_lap['LapNumber']}")
+print(f"\t Fastest Sector 1: {fast_lap['Sector1Time']}")
+print(f"\t Fastest Sector 2: {fast_lap['Sector2Time']}")
+print(f"\t Fastest Sector 3: {fast_lap['Sector3Time']}")
